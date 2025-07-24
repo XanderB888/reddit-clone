d9a99cbc198b3deb2f10f5458e8d89717b53f30e
Xander <xander.cage.botha@gmail.com>
2025-07-24 21:22:42 +0100

    import Header to Homepage.js as function

---
+++ src/pages/Home/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+
+function HomePage() {
+    return (
+        <>
+            <Header />
+            <main style={{ padding: '2rem'}}>
+                <h2>Home feed</h2>
+            </main>
+        </>
+    );
+}
+
+export default HomePage;
