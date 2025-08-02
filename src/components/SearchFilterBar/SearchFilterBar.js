ba6347308dd983f4c666743ee8f8c1e15faf24f3
Xander <xander.cage.botha@gmail.com>
2025-08-02 17:43:06 +0100

    Update SearchFIlterBar.js to Use Props

--- src/components/SearchFilterBar/SearchFilterBar.js
+++ src/components/SearchFilterBar/SearchFilterBar.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './SearchFilterBar.css';
 
-function SearchFilterBar() {
+function SearchFilterBar({ searchTerm, setSearchTerm, filterCategory, setFilterCategory }) {
     return (
         <div className='search-filter-bar'>
             <input
                 type="text"
                 placeholder="Search Posts..."
                 className='search-input'
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
             />
-            <select className='filter-dropdown'>
+            <select 
+                className='filter-dropdown'
+                value={filterCategory}
+                onChange={(e) => setFilterCategory(e.target.value)}
+                >
                 <option value="">All Categories</option>
                 <option value="">Funny</option>
                 <option value="">News</option>
