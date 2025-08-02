709b86b960cc095a1dc314e6a19fa11146cb4bd6
Xander <xander.cage.botha@gmail.com>
2025-08-02 17:21:27 +0100

    Update HomePage.js to store state for searchterm & filterCategory

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/Header/Header';
 import Ticker from '../../components/Ticker/Ticker';
 import PostList from '../../components/PostList/PostList';
 import SearchFilterBar from '../../components/SearchFilterBar/SearchFilterBar';
 
 function HomePage() {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filterCategory, setFilterCategory] = useState('');
+
     return (
         <>
             <Header />
             <Ticker />
             <main style={{ padding: '2rem'}}>
-                <SearchFilterBar />
-                <PostList />
+                <SearchFilterBar 
+                    searchTerm={searchTerm}
+                    setSearchTerm={setSearchTerm}
+                    filterCategory={filterCategory}
+                    setFilterCategory={setFilterCategory}
+                />
+                <PostList 
+                    searchTerm={searchTerm}
+                    filterCategory={filterCategory}
+                />
             </main>
         </>
     );
