06f69071b1a0edb0c6349ee0b67c6721a316c9d5
Xander <xander.cage.botha@gmail.com>
2025-08-02 16:26:00 +0100

    create body for PostCard.js

---
+++ src/components/PostCard/PostCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../PostCard.css';
+
+function Postcard({ post }) {
+    return (
+        <div className='post-card'>
+            <h3>{post.title}</h3>
+            <div className='post-meta'>
+                <span>{post.subreddit}</span>
+                <span>• Posted by u/{post.author}</span>
+                <span>• 💬 {post.comments} comments</span>
+                <span>• ⬆️ {post.score}</span>
+            </div>
+        </div>
+    );
+}
+
+export default Postcard;
