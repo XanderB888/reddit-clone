98362beeb9fd10ce96070f79f580f22750861136
Xander <xander.cage.botha@gmail.com>
2025-08-02 16:20:20 +0100

    Create PostList.js, PostList.css, PostCard.js & PostCard.css - create PostList.js body with mock data

---
+++ src/components/PostList/PostList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../PostList.css';
+import PostCard from '../PostCard/PostCard';
+
+function PostList () {
+    //Temp mock data
+    const mockPosts = [
+        {
+            id:1,
+            title: 'This is a post about cats',
+            subreddit: 'r/aww',
+            author: 'catlover92',
+            comments: 888,
+            score: 342,
+        },
+        {
+            id:2,
+            title: 'Ask Me Anything: Space engineer',
+            subreddit: 'r/space',
+            author: 'spacegeek',
+            comments: 99,
+            score: 701,
+        },
+    ];
+
+    return (
+        <div className='post-list'>
+            {mockPosts.map((post) => (
+                <PostCard key={post.id} post={post} />
+            ))}
+        </div>
+    );
+}
+
+export default PostList;
