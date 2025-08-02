9680b02b312e3e0850d34e04132dc2462da4e67a
Xander <xander.cage.botha@gmail.com>
2025-08-02 17:01:54 +0100

    Add SearchFilterBar component to HomePage.js

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../../components/Header/Header';
 import Ticker from '../../components/Ticker/Ticker';
 import PostList from '../../components/PostList/PostList';
+import SearchFilterBar from '../../components/SearchFilterBar/SearchFilterBar';
 
 function HomePage() {
     return (
@@ -9,6 +10,7 @@ function HomePage() {
             <Header />
             <Ticker />
             <main style={{ padding: '2rem'}}>
+                <SearchFilterBar />
                 <PostList />
             </main>
         </>
