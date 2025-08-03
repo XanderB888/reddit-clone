b78e2f61cd0a451f599f175dcdaced6d3d67b24d
Xander <xander.cage.botha@gmail.com>
2025-08-03 21:29:22 +0100

    added export to PostCard.js and import on App.js to fix small bug - Posts are now clickable

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage';
+import HomePage from './pages/Home/HomePage';
 import PostDetail from './pages/PostDetail/PostDetail';
 
 function App() {
--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -17,3 +17,5 @@ function PostCard({ post }) {
     </div>
   );
 }
+
+export default PostCard;
