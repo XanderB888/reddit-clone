9659db050574599f22bc4d678796c12ebdc23726
Xander <xander.cage.botha@gmail.com>
2025-08-09 17:36:55 +0100

    update PostList.js to replace mock data with fetchPosts from postsSlice

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -1,53 +1,27 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { setPosts } from '../../features/posts/postsSlice';
+import { fetchPosts } from '../../features/posts/postsSlice';
 import './PostList.css';
 import PostCard from '../PostCard/PostCard';
 
 function PostList () {
     const dispatch = useDispatch();
-    const posts = useSelector((state) => state.posts.items);
+    const { items: posts, status } = useSelector((state) => state.posts);
     
     useEffect(() => {
-        //Temp mock posts untill I connect the Reddit API
-    const mockPosts = [
-        {
-            id:1,
-            title: 'This is a post about cats',
-            subreddit: 'aww',
-            author: 'catlover92',
-            comments: 888,
-            score: 342,
-        },
-        {
-            id:2,
-            title: 'Ask Me Anything: Space engineer',
-            subreddit: 'space',
-            author: 'spacegeek',
-            comments: 99,
-            score: 701,
-        },
-        {
-            id:3,
-            title: 'Funniest Gaming Moments',
-            subreddit: 'gaming',
-            author: 'proGamer',
-            comments: 44,
-            score: 108,
-        },
-        {
-            id:4,
-            title: 'Ultimate Fails of 2025 July',
-            subreddit: 'funny',
-            author: 'UF Guy',
-            comments: 288,
-            score: 333,
+        if (status === 'idle') {
+            dispatch(fetchPosts());
         }
-    ];
+    }, [status, dispatch]);
 
-    dispatch(setPosts(mockPosts));
-    }, [dispatch]);   
+    if (status === 'loading') {
+        return <p>Loading posts...</p>;
+    }
 
+    if (status === 'failed') {
+        return <p>Failed to load posts.</p>
+    }
+      
     return (
         <div className='post-list'>
             {posts.map((post) => (
